49a6cb2eff1f546c618fd304f18524ac5909ff7d
Harry Zumwalt <harry.zum@gmail.com>
2017-02-07 15:17:26 -0600

    added ability to load a random fen position from pgn file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,54 +1,128 @@
 $(document).ready(function() {
-    var board
-    game = new Chess();
+  pgn = ['[Event "Casual Game"]',
+       '[Site "Berlin GER"]',
+       '[Date "1852.??.??"]',
+       '[EventDate "?"]',
+       '[Round "?"]',
+       '[Result "1-0"]',
+       '[White "Adolf Anderssen"]',
+       '[Black "Jean Dufresne"]',
+       '[ECO "C52"]',
+       '[WhiteElo "?"]',
+       '[BlackElo "?"]',
+       '[PlyCount "47"]',
+       '',
+       '1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.b4 Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O',
+       'd3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4',
+       'Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 17.Nf6+ gxf6 18.exf6',
+       'Rg8 19.Rad1 Qxf3 20.Rxe7+ Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8',
+       '23.Bd7+ Kf8 24.Bxe7# 1-0'];
 
-  // do not pick up pieces if the game is over
-  // only pick up pieces for White
-  var onDragStart = function(source, piece, position, orientation) {
-    if (game.in_checkmate() === true || game.in_draw() === true ||
-      piece.search(/^b/) !== -1) {
-      return false;
+
+const chess1 = new Chess();
+const chess2 = new Chess();
+const startPos = chess2.fen();
+
+chess1.load_pgn(pgn.join('\n'));
+let fens = chess1.history().map(move => {
+  chess2.move(move);
+  return chess2.fen();
+});
+
+//the above technique will not capture the fen of the starting position.  therefore:
+fens = [startPos, ...fens];
+
+//double checking everything
+fens.forEach(fen => console.log(fen));
+
+//logic for getting random fen ***********************
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  randomkey = getRandomArbitrary(10, fens.length)
+  console.log("randomkey", randomkey)
+  randomfen = fens[randomkey]
+  console.log("randomfen", randomfen)
+
+//Board Creation and rules *****************************
+var board,
+  game = new Chess(randomfen),
+  statusEl = $('#status'),
+  fenEl = $('#fen'),
+  pgnEl = $('#pgn');
+
+// do not pick up pieces if the game is over
+// only pick up pieces for the side to move
+var onDragStart = function(source, piece, position, orientation) {
+  if (game.game_over() === true ||
+      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false;
+  }
+};
+
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+
+  updateStatus();
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+var updateStatus = function() {
+  var status = '';
+
+  var moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
     }
-  };
+  }
 
-  var makeRandomMove = function() {
-    var possibleMoves = game.moves();
+  statusEl.html(status);
+  fenEl.html(game.fen());
+  pgnEl.html(game.pgn());
+};
 
-    // game over
-    if (possibleMoves.length === 0) return;
+var cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+board = ChessBoard('board', cfg);
+board.position(randomfen)
 
-    var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    game.move(possibleMoves[randomIndex]);
-    board.position(game.fen());
-  };
-
-  var onDrop = function(source, target) {
-    // see if the move is legal
-    var move = game.move({
-      from: source,
-      to: target,
-      promotion: 'q' // NOTE: always promote to a queen for example simplicity
-    });
-
-    // illegal move
-    if (move === null) return 'snapback';
-
-    // make random legal move for black
-    window.setTimeout(makeRandomMove, 250);
-  };
-
-  // update the board position after the piece snap
-  // for castling, en passant, pawn promotion
-  var onSnapEnd = function() {
-    board.position(game.fen());
-  };
-
-  var cfg = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
-  };
-  board = ChessBoard('board', cfg);
+updateStatus();
 })
