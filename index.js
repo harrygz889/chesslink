dca8baaa14418e324c8b8bba1fb649c200be7b76
Harry Zumwalt <harry.zum@gmail.com>
2017-02-06 20:03:01 -0600

    change index.js to send html

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(request, response) {
-  response.render('index.html');
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
 app.listen(app.get('port'), function() {
